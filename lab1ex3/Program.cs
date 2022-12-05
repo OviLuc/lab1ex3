77b2b0f4856d8bd741c8e9b225861916e9677c33
Lucaci <Lucaci@DESKTOP-0E3OOL2>
2022-12-05 21:04:44 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa ultima cifra a unui numar intreg */
+
+            
+           Console.WriteLine("Acest program va afisa ultima cifra a unui numar intreg");
+           
+            Console.WriteLine("Introduceti numarul");
+            int x = int.Parse(Console.ReadLine());
+
+            int y = x % 10;
+            Console.WriteLine( "Ultima cifra este " + y);
+        }
+    }
+}
